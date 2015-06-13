f79349028482daae78087ef062aecf44695fadd8
umemotoseiki <umeme0917@gmail.com>
2015-06-13 15:47:43 +0900

    test commit

--- app/src/main/java/com/example/umeme/myapplication/MainActivity.java
+++ app/src/main/java/com/example/umeme/myapplication/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    //テスト
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
